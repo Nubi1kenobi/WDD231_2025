6bc5d99578d8e45accfa1b6493c9a3b250b170e1
Bryan C. Stauch <sta15060@byui.edu>
2025-09-23 21:43:50 -0500

    week1examples

---
+++ whateveryoulike.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 2;
+const total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("myAppleElement").textContent = friendApples;
+document.getElementById("myAppleElement").textContent = total;
