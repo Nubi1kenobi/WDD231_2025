d6b3362f75d7313f55cfe41e645d37bc301f9b18
Bryan C. Stauch <sta15060@byui.edu>
2025-09-23 21:50:40 -0500

    weekoneexamples2

--- whateveryoulike.js
+++ whateveryoulike.js
@@ -1,7 +1,7 @@
 const myApples = 4;
 const friendApples = 2;
-const total = myApples + friendApples;
+let total = myApples + friendApples;
 
 document.getElementById("myAppleElement").textContent = myApples;
-document.getElementById("myAppleElement").textContent = friendApples;
-document.getElementById("myAppleElement").textContent = total;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalAppleElement").textContent = total;
